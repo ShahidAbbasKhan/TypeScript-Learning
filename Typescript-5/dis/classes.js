675945925e1031e1c4ba9731f038492be6c1c376
ShahidAbbasKhan <developershahid297@gmail.com>
2022-12-27 21:34:19 +0500

    minor changes

--- Typescript-5/dis/classes.js
+++ Typescript-5/dis/classes.js
@@ -18,4 +18,29 @@ class instagram {
         return true;
     }
 }
-//Abstract Class
+//Abstract Class (can't initize object from abstract class)
+class TakePhoto {
+    constructor(cameraMode, filter, brust) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.brust = brust;
+    }
+    cameraImplemented() {
+        console.log("Camera is Implemented");
+    }
+}
+// const absObj= new TakePhoto("OK","OK",7); //Error Cannot create an instance of an abstract class.
+class Facebook extends TakePhoto {
+    constructor(cameraMode, filter, brust, cameraName) {
+        super(cameraMode, filter, brust);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.brust = brust;
+        this.cameraName = cameraName;
+    }
+    cameraWorking() {
+        return true;
+    }
+}
+const fbObject = new Facebook("OK", "OK", 7, "New camera");
+//Generics (malke components to Reuseable)
--- Typescript-5/src/classes.ts
+++ Typescript-5/src/classes.ts
@@ -25,5 +25,41 @@ class instagram implements getCamera,Story {
 }
 
 
-//Abstract Class
+//Abstract Class (can't initize object from abstract class)
 
+abstract class TakePhoto{
+    constructor(
+        public cameraMode:string,
+        public filter:string,
+        public brust:number
+    ){}
+    abstract cameraWorking():boolean;
+
+    cameraImplemented():void{
+        console.log("Camera is Implemented");
+    }
+ 
+    
+}
+// const absObj= new TakePhoto("OK","OK",7); //Error Cannot create an instance of an abstract class.
+
+class Facebook extends TakePhoto {
+    constructor( 
+        public cameraMode:string,
+        public filter:string,
+        public brust:number,
+        public cameraName:string
+    )
+    {super(cameraMode,filter,brust)}
+    cameraWorking(): boolean {
+        return true;
+    }
+    
+}
+ const fbObject= new Facebook("OK","OK",7,"New camera");
+
+
+
+ //Generics (malke components to Reuseable)
+
+ 
